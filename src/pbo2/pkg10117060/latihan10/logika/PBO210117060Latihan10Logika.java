6a48cb38c666b35ebd7ff3b31e94152f89f04dcc
kekerofficial <43506956+kekerofficial@users.noreply.github.com>
2018-09-23 12:30:51 +0700

    Add files via upload

---
+++ src/pbo2/pkg10117060/latihan10/logika/PBO210117060Latihan10Logika.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117060.latihan10.logika;
+
+/**
+ *
+* @author 
+ * NAMA : Irfan Faisal Hutama Tanjung.
+ * KELAS : PBO2
+ * NIM : 10117060
+ * Deskripsi Program : Program ini berisi tentang logika yang dimana
+ * jawabannnya hanya ada 2. yaitu true atau false.
+ */
+public class PBO210117060Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
